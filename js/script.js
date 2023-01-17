7ac1bd8ac7c73b26ff09921e4c6086fd0b50e1f7
Viktor <364185@protonmail.com>
2023-01-17 15:01:07 +0400

    check for victory in 3 rows horizontally

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-    
+
 const grid = [
     [0, 0, 0],
     [0, 0, 0],
@@ -8,15 +8,30 @@ const grid = [
 
 function renderGrid(grid) {
     for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
-        
+        /* if(statechek === false) {
+             console.log('сколько иксов');
+             winX = winX + 1
+             if(winX < 3) {
+                 alert('X WIN!')
+             }
+         }
+         if(statechek === true) {
+             winY = winY + 1
+             if(winY < 3) {
+                 alert('O WIN!')
+             }
+         } */
         for (let cellIndex = 0; cellIndex < grid[rowIndex].length; cellIndex++) {
+
             renderCell(cellIndex, rowIndex, grid[rowIndex][cellIndex], 100)
+            console.log('grid[' + rowIndex + '][' + cellIndex + '] =' + grid[rowIndex][cellIndex])
+            //console.log(grid[rowIndex][cellIndex])
+
         }
-        
     }
 }
 
-function renderCell(cellIndex, rowIndex, state = 0, size = 50) {
+function renderCell(cellIndex, rowIndex, state, size = 50) {
     const newDiv = document.createElement("div");
     newDiv.className = 'cell';
     newDiv.style.left = cellIndex * size + "px"
@@ -36,29 +51,44 @@ function renderCell(cellIndex, rowIndex, state = 0, size = 50) {
 
 
     document.body.appendChild(newDiv);
-    
+
 }
+
+let numberOfCalls = 0;
 
 document.addEventListener('click', (event) => {
     const rowIndex = event.target.dataset.row
     const cellIndex = event.target.dataset.cell
-    
+
     let fn = (function() {
-        let numberOfCalls = 0;
         return function() {
-           return Number.isInteger(++ numberOfCalls / 2)
+            return Number.isInteger(++ numberOfCalls / 2)
         }
-     })();
+    })();
 
     grid[rowIndex][cellIndex] = fn()
-   
-   
-   
-   
-   
-   
-   
+
     renderGrid(grid)
+
+    if(grid[0][0] === false && grid[0][1] === false && grid[0][2] === false) {
+        alert('X is the winner!')
+    }
+    if(grid[0][0] === true && grid[0][1] === true && grid[0][2] === true) {
+        alert('O is the winner!')
+    }
+    if(grid[1][0] === false && grid[1][1] === false && grid[1][2] === false) {
+        alert('X is the winner!')
+    }
+    if(grid[1][0] === true && grid[1][1] === true && grid[1][2] === true) {
+        alert('O is the winner!')
+    }
+    if(grid[2][0] === false && grid[2][1] === false && grid[2][2] === false) {
+        alert('X is the winner!')
+    }
+    if(grid[2][0] === true && grid[2][1] === true && grid[2][2] === true) {
+        alert('O is the winner!')
+    }
 })
+
 
 renderGrid(grid)
